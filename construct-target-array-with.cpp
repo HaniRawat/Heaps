31deb039df6dd4b5a2663606666bc7a6455fc331
HaniRawat <120402194+HaniRawat@users.noreply.github.com>
2024-07-16 21:58:57 +0530

    solved

---
+++ construct-target-array-with.cpp
@@ -0,0 +1,25 @@
+lass Solution {
+public:
+    bool isPossible(vector<int>& target) {
+        priority_queue<int>pq;
+        unsigned int sum = 0;
+        for(int x : target ){
+            sum += x;
+            pq.push(x);
+        }
+
+        while(pq.top() != 1){
+            int x = pq.top();
+            pq.pop();
+
+            sum -= x;
+
+            if(sum >= x || sum < 1)
+                return false;
+            x %= sum;
+            sum += x;
+            pq.push(x? x: sum);
+        }
+        return true;
+    }
+};
